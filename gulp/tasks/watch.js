bf3943361052e285e92a2467002e1083d0938d47
xamjo <xamjofoux@gmail.com>
2016-09-28 00:08:21 +0300

    nothing changed

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,7 +5,10 @@ module.exports = function() {
     $.gulp.watch('./source/js/**/*.js', $.gulp.series('js:process'));
     $.gulp.watch('./source/style/**/*.scss', $.gulp.series('sass'));
     $.gulp.watch('./source/template/**/*.pug', $.gulp.series('pug'));
-    $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
+    $.gulp.watch([
+      './source/images/**/*.*',
+      '!./source/images/spriteSourse/**/*.*'
+	], $.gulp.series('copy:image'));
     $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy:font'));
   });
 };
