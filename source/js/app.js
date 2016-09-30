b5576026de80906dbcf1efab68918ff9f84a39fa
xamjo <xamjofoux@gmail.com>
2016-09-30 19:10:19 +0300

    index page almost done

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,13 @@
 (function() {
   'use strict';
-
-  setTimeout(function() {
-    document.querySelector('.greating_picture').classList.add('m--show');
-  }, 1000);
+   $(".autorization__buttonHolder_button").on("click", function(){
+    $(".mainContainer__wideWrapper").addClass("mainContainer__wideWrapper_moved");
+    var that = this;
+    $(that).fadeTo(1000,0);
+    setTimeout(function() {$(that).css("display","none"); console.log(that);}, 1000);
+  });
+   $(".mainContainer__block_links_home").on("click", function(){
+    $(".mainContainer__wideWrapper").removeClass("mainContainer__wideWrapper_moved");
+    $(".autorization__buttonHolder_button").css("display","inline-block").fadeTo(1000,1);
+  });
 })();
