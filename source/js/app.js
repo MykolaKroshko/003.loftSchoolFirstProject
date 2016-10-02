9c35b88a63d5812d7b4dda7e9d2f19f958f7dbc1
xamjo <xamjofoux@gmail.com>
2016-10-02 17:15:07 +0300

    static site seems to be done

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,13 @@
 (function() {
   'use strict';
    $(".autorization__buttonHolder_button").on("click", function(){
-    $(".mainContainer__wideWrapper").addClass("mainContainer__wideWrapper_moved");
+    $(".mainContainer").addClass("hover");
     var that = this;
     $(that).fadeTo(1000,0);
     setTimeout(function() {$(that).css("display","none"); console.log(that);}, 1000);
   });
    $(".mainContainer__block_links_home").on("click", function(){
-    $(".mainContainer__wideWrapper").removeClass("mainContainer__wideWrapper_moved");
+    $(".mainContainer").removeClass("hover");
     $(".autorization__buttonHolder_button").css("display","inline-block").fadeTo(1000,1);
   });
 })();
---
+++ source/js/blogScroll.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  showArticle(window.location.hash, false);
+
+  $(".nav__list_link").on("click",function(e){
+    e.preventDefault();
+    showArticle($(this).attr("href"),true);
+  })
+});
+
+
+
+$(window).scroll(function(){
+  checkArticle();
+});
+
+function showArticle(article, isAnimated){
+  var
+    reqArticle = $(".blog__article").filter('[data-article="'+article.replace(/#/,"")+'"]');
+    position = reqArticle.offset().top-50;
+    console.log(reqArticle, position);
+  if (isAnimated){
+    $('body, html').animate({scrollTop: position}, 500);
+  } else {
+    $('body, html').scrollTop(position);
+  }
+
+}
+
+function checkArticle(){
+  $(".blog__article").each(function(){
+    var
+      that = $(this),
+      top = that.offset().top-50,
+      bottom = top+that.height(),
+      scroll = $(window).scrollTop();
+    if(top < scroll && bottom > scroll){
+      var
+        currrentArticle = that.data("article"),
+      reqLink = $(".nav__list_link").filter('[href="#'+currrentArticle+'"]');
+      reqLink.closest(".nav__list_item").addClass("active").siblings().removeClass("active");
+      location.hash = currrentArticle;
+    }
+  });
+}
