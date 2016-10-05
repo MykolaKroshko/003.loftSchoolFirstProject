3c4813be2e25c90d3f648a7494e9a274955b755c
xamjo <xamjofoux@gmail.com>
2016-10-05 14:59:54 +0300

    index page pesponsivity added

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
-   $(".autorization__buttonHolder_button").on("click", function(){
+   $(".autorization__button").on("click", function(){
     $(".mainContainer").addClass("hover");
     var that = this;
     $(that).fadeTo(1000,0);
@@ -8,6 +8,6 @@
   });
    $(".mainContainer__block_links_home").on("click", function(){
     $(".mainContainer").removeClass("hover");
-    $(".autorization__buttonHolder_button").css("display","inline-block").fadeTo(1000,1);
+    $(".autorization__button").css("display","inline-block").fadeTo(1000,1);
   });
 })();
