28b1711f8f1bb45cbd19633e63bd653ec7d4b297
root <root@testubuntu.ignite.local>
2016-10-18 21:05:50 +0300

    server update

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 var express = require('express');
+var parser = require('body-parser');
+var mongo = require('mongoose');
+
 var app = express();
+var jsonParse = parser.json();
+mongo.connect('mongodb://localhost/portfolioDB',{});
+
+var Mail = mongo.model('Mail',{
+    name: String,
+    mail: String,
+    massage: String
+});
 
 app.set('views', './build/templates');
 app.set('view engine', 'pug');
 
 // routes
 
-app.get('/',function(req, res){
-    res.send('Hello world!');
+app.use(express.static('./build'));
+
+//app.get('/',function(req, res){
+//    res.send('Hello world!');
+//});
+
+app.post('/saveMail', function(req, res){
+    console.log('mail');
+    //var mail = new Mail(req.body);
+//    mail.save(function(err){
+//	if (err){
+//	    res.send('error'+err);
+//	}else{
+//	    res.send('mail sent')
+//	}
+//    });
 });
 
 // run server
