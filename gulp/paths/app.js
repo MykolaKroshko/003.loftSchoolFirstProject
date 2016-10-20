23c444017d9f6c8795bc472cfc46c306ebcdfbc8
xamjo <xamjofoux@gmail.com>
2016-10-20 18:22:33 +0300

    burger menu added

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -4,6 +4,7 @@ module.exports = [
   './source/js/app.js',
   './source/js/preload.js',
   './source/js/paralax.js',
+  './source/js/humMenu.js',
   './source/js/slider.js',
   './source/js/blogScroll.js',
   './source/js/validation.js',
